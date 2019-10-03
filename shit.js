6113553048eb7f54f095d7298600e92eedf2ec28
salmanmarizki <salmanmarizki@outlook.com>
2019-10-03 12:11:56 +0800

    More updates

---
+++ shit.js
@@ -0,0 +1,824 @@
+cards = [
+{
+  "id": 1,
+  "description": "WOW! So you're taking over after Elon Bust?! We gotta celebrate!",
+  "character": "Best Friend EVAR",
+  "conditions": "",
+  "choices": {
+   "a": {
+    "label": "We gonna party so hard we won't remember a thing!",
+    "values": {
+     "money": -5,
+     "environment": -5,
+     "time": -1,
+    }
+   },
+   "b": {
+    "label": "I'm still settling in with this new responsibilty",
+    "values": {
+     "money": 5,
+     "environment": 0,
+     "time": -1,
+    }
+   }
+  }
+ },
+{
+  "id": 2,
+  "description": "Will you sponsor my run to save the whales?",
+  "character": "Girl Scout",
+  "conditions": "",
+  "choices": {
+   "a": {
+    "label": "Is $10M too much?",
+    "values": {
+     "money": -10,
+     "environment": 5,
+     "time": 0,
+    }
+   },
+   "b": {
+    "label": "Have you ever tried whale meat?",
+    "values": {
+     "money": 0,
+     "environment": -5,
+     "time": 0,
+    }
+   }
+  }
+ },
+{
+  "id":3,
+  "description": "All but two penguin species are now officially on the endangered list.",
+  "character": "World Renown Animal Expert",
+  "conditions": "",
+  "choices": {
+   "a": {
+    "label": "Build a penguin conservatorium.",
+    "values": {
+     "money": -30,
+     "environment": 5,
+     "time": -3,
+    }
+   },
+   "b": {
+    "label": "Start 'Penguin Flinging' as an official olympic sport.",
+    "values": {
+     "money": 30,
+     "environment": -10,
+     "time": -2,
+    }
+   }
+  }
+ },
+{
+  "id":4,
+  "description": "The GMO vs Organic crops debate continues...",
+  "character": "News Reader",
+  "conditions": "",
+  "choices": {
+   "a": {
+    "label": "All my company cafeterias will sell only Organic.",
+    "values": {
+     "money": -10,
+     "environment": -5,
+     "time": -1,
+    }
+   },
+   "b": {
+    "label": "Whatever... food is food.",
+    "values": {
+     "money": 0,
+     "environment": 5,
+     "time": -1,
+    }
+   }
+  }
+ },
+{
+  "id":5,
+  "description": "This is perfect timing on the lithium mining acquisition! Battery orders are growing faster than ever before.",
+  "character": "Financial Analyst",
+  "conditions": "",
+  "choices": {
+   "a": {
+    "label": "Increase prices to manage mining rates.",
+    "values": {
+     "money": 20,
+     "environment": 0,
+     "time": -1,
+    }
+   },
+   "b": {
+    "label": "Drill that sh#% and lets make some mow-nay!",
+    "values": {
+     "money": 40,
+     "environment": -10,
+     "time": -2,
+    }
+   }
+  }
+ },
+{
+  "id":6,
+  "description": "Congratulations on the new battery technology patent! But we have a perfect monopoly on the lithium battery market.",
+  "character": "Business Analyst",
+  "conditions": "",
+  "choices": {
+   "a": {
+    "label": "Let's start the roll out asap!",
+    "values": {
+     "money": -50,
+     "environment": 5,
+     "time": -2,
+    }
+   },
+   "b": {
+    "label": "It's OUR secret until we've sold all the lithium we can.",
+    "values": {
+     "money": 50,
+     "environment": -10,
+     "time": -3,
+    }
+   }
+  }
+ },
+{
+  "id":7,
+  "description": "Population growth continues to rise exponentially and overpopulation is a growing concern.",
+  "character": "NASA",
+  "conditions": "",
+  "choices": {
+   "a": {
+    "label": "Launch 'Skynet' brand condoms.",
+    "values": {
+     "money": 20,
+     "environment": 5,
+     "time": -2,
+    }
+   },
+   "b": {
+    "label": "Program a 'natural selection' algorithm into Tesla autopilot",
+    "values": {
+     "money": -20,
+     "environment": 5,
+     "time": -2,
+    } //set condition for salman's sue question.
+   }
+  }
+ },
+{
+  "id":8,
+  "description": "How can we make Mars habitable?",
+  "character": "Talk Show Host",
+  "conditions": "",
+  "choices": {
+   "a": {
+    "label": "The fastest way is to drop nukes on either pole.",
+    "values": {
+     "money": -50,
+     "environment": 10,
+     "time": -3,
+    }
+   },
+   "b": {
+    "label": "Release green house gas into it, it'll get warm",
+    "values": {
+     "money": -30,
+     "environment": 0,
+     "time": -3,
+    }
+   }
+  }
+ },
+{
+  "id":9,
+  "description": "We want your endorsement for our product.",
+  "character": "CEO of Affordable Denim Co.",
+  "conditions": "",
+  "choices": {
+   "a": {
+    "label": "You mean my product. I just bought your company.",
+    "values": {
+     "money": 30,
+     "environment": -15,
+     "time": -2,
+    }
+   },
+   "b": {
+    "label": "Stop pouring your chemicals in the community waterways.",
+    "values": {
+     "money": 0,
+     "environment": 0,
+     "time": 0,
+    }
+   }
+  }
+ },
+{
+  "id":10,
+  "description": "How can we best bridge the gap between green energy supply and energy consumption?",
+  "character": "Talk Show Host",
+  "conditions": "",
+  "choices": {
+   "a": {
+    "label": "Nuclear for the motherf#$%ing win!",
+    "values": {
+     "money": 40,
+     "environment": -10,
+     "time": -3,
+    } //set condition for question 31
+   },
+   "b": {
+    "label": "There is still a lot of untapped potential in wind and tidal energy.",
+    "values": {
+     "money": -50,
+     "environment": 10,
+     "time": -3,
+    }
+   }
+  }
+ },
+{
+"id": 11,
+"description": "Pertolas Oil Co. has a great project",
+"character": "Office-Manager",
+"conditions": "",
+"choices": {
+"a": {
+"label": "What are we waiting for!",
+"values": {
+"money": 30,
+"environment": -10,
+"time": -3,
+}
+},
+"b": {
+"label": "Ignore",
+"values": {
+"money": 0,
+"environment": -10,
+"time": 0,
+}
+}
+}
+},
+{
+"id": 12,
+"description": "NGR Energy company wants to build a nuclear power plant on one of your lands",
+"character": "Office-Manager",
+"conditions": "",
+"choices": {
+"a": {
+"label": "Let's Party",
+"values": {
+"money": 40,
+"environment": -5,
+"time": -3,
+}
+},
+"b": {
+"label": "'cries in the corner'",
+"values": {
+"money": 0,
+"environment": 0,
+"time": -1,
+}
+}
+}
+},
+{
+"id": 13,
+"description": "Heard rumours that Gonold Trunk wants to build more buildings by destroying more trees",
+"character": "John, My Partner in Crime",
+"conditions": "",
+"choices": {
+"a": {
+"label": "Let's Protest",
+"values": {
+"money": -5,
+"environment": 10,
+"time": -1,
+}
+},
+"b": {
+"label": "Get the car ready",
+"values": {
+"money": 30,
+"environment": -5,
+"time": -3,
+}
+}
+}
+},
+{
+"id": 14,
+"description": "National Zoo reported that there is one tiger left in the US",
+"character": "Anna, The National Zoo Reporter",
+"conditions": "",
+"choices": {
+"a": {
+"label": "Cars are faster than tigers.",
+"values": {
+"money": 10,
+"environment": -10,
+"time": 0,
+}
+},
+"b": {
+"label": "Initiate X-breading project with bengal cats",
+"values": {
+"money": -20,
+"environment": 10,
+"time": -3,
+}
+}
+}
+},
+{
+"id": 15,
+"description": "A music band needs sponsor for their music video",
+"character": "Tom, The Guitarist",
+"conditions": "",
+"choices": {
+"a": {
+"label": "Let's do it",
+"values": {
+"money": -5,
+"environment": 0,
+"time": 0,
+}
+},
+"b": {
+"label": "Don't give a f&%#!",
+"values": {
+"money": 0,
+"environment": 0,
+"time": 0,
+}
+}
+}
+},
+{
+"id": 16,
+"description": "Fund raiser comes to you to save trees",
+"character": "Johnny, The Principal",
+"conditions": "",
+"choices": {
+"a": {
+"label": "Ignore",
+"values": {
+"money": 0,
+"environment": -5,
+"time": 0,
+}
+},
+"b": {
+"label": "Get me my wallet",
+"values": {
+"money": -5,
+"environment": 5,
+"time": 0,
+}
+}
+}
+},
+{
+"id": 17,
+"description": "Got a lot of used tires",
+"character": "Office-Manager",
+"conditions": "",
+"choices": {
+"a": {
+"label": "Let's make a Test-Lah drift team",
+"values": {
+"money": 10,
+"environment": -5,
+"time": -2,
+}
+},
+"b": {
+"label": "Let's reprocess it into car interior fabric",
+"values": {
+"money": -20,
+"environment": 10,
+"time": -3,
+}
+}
+}
+},
+{
+"id": 18,
+"description": "A staff got severely injured in a factory",
+"character": "Tommy, The Worker",
+"conditions": "",
+"choices": {
+"a": {
+"label": "Kill him to avoid compensating him", //condition for q19
+"values": {
+"money": 0,
+"environment": 5,
+"time": 0,
+}
+},
+"b": {
+"label": "Visit him in the hospital",
+"values": {
+"money": -10,
+"environment": 0,
+"time": 0,
+}
+}
+}
+},
+
+{
+"id": 19,
+"description": "What do we do with the body?",
+"character": "Tommy, The Worker",
+"conditions": "",
+"choices": {
+"a": {
+"label": "Feed him to your Pet Alligator",
+"values": {
+"money": 5,
+"environment": 5,
+"time": 0,
+}
+},
+"b": {
+"label": "Dissolve it in Acid",
+"values": {
+"money": -5,
+"environment": -5,
+"time": 0,
+}
+}
+}
+},
+{
+  "id":20,
+  "description": "How can we reduce nuclear waste?",
+  "character": "President of Japan",
+  "conditions": "",
+  "choices": {
+   "a": {
+    "label": "Launch them as missiles aimed at the poles of Mars.",
+    "values": {
+     "money": -50,
+     "environment": 10,
+     "time": -3,
+    } //condition for question 8
+   },
+   "b": {
+    "label": "Nuclear energy is only a temporary filler for the gaps in our green energy consumption",
+    "values": {
+     "money": -50,
+     "environment": 10,
+     "time": 0,
+    }
+   }
+  }
+ },
+{
+    "id": 21,
+    "description": "Customers are suing Test-Lah because they crashed while engaging AutoPilot, sir.",
+    "character": "JB Straubel, CTO",
+    "conditions": "",
+    "choices": {
+      "a": {
+        "label": "Hah! Natural selection at its best!",
+        "values": {
+          "money": -20,
+          "environment": 5,
+          "time": 0,
+        }
+      },
+      "b": {
+        "label": "Call my f*cking lawyers!",
+        "values": {
+          "money": -20,
+          "environment": 5,
+          "time": -2,
+        }
+      }
+    }
+  },
+{
+    "id": 22,
+    "description": "Your Test-Lah Soup-R-Chargers are slowly burning down, sir.",
+    "character": "Jason Wheeler, CFO",
+    "conditions": "",
+    "choices": {
+      "a": {
+        "label": "Rebuild them!",
+        "values": {
+          "money": 10,
+          "environment": 10,
+          "time": -2,
+        }
+      },
+      "b": {
+        "label": "Michael Gay action time! Grab the popcorn!",
+        "values": {
+          "money": -20,
+          "environment": -10,
+          "time": 0,
+        }
+      }
+    }
+  },
+{
+    "id": 23,
+    "description": "Can you help me build a fully autonomous army?",
+    "character": "Donald Trump, President of USA",
+    "conditions": "",
+    "choices": {
+      "a": {
+        "label": "You fo' reelz Esse?!",
+        "values": {
+          "money": 0,
+          "environment": 0,
+          "time": 0,
+        }
+      },
+      "b": {
+        "label": "Let me get to my drawing board.",
+        "values": {
+          "money": 30,
+          "environment": -5,
+          "time": -3,
+        }
+      }
+    }
+  },
+{
+    "id": 24,
+    "description": "I need some autonomous vehicles for mining purposes, would you care to develop one for me?",
+    "character": "Coral",
+    "conditions": "",
+    "choices": {
+      "a": {
+        "label": "NO COAL, NO FOSSIL FUEL, GO GREEN",
+        "values": {
+          "money": 0,
+          "environment": 0,
+          "time": 0,
+        }
+      },
+      "b": {
+        "label": "Good deal.",
+        "values": {
+          "money": 30,
+          "environment": -10,
+          "time": -2,
+        }
+      }
+    }
+  },
+{
+    "id": 25,
+    "description": "We need to build more Biga-factories, sir.",
+    "character": "JB Straubel, CTO",
+    "conditions": "",
+    "choices": {
+      "a": {
+        "label": "The f&^# you waiting for? Get to work!",
+        "values": {
+          "money": 10,
+          "environment": -5,
+          "time": -3,
+        }
+      },
+      "b": {
+        "label": "Costs too much.",
+        "values": {
+          "money": 0,
+          "environment": 0,
+          "time": 0,
+        }
+      }
+    }
+  },
+{
+    "id": 26,
+    "description": "We need to expand our solar department.",
+    "character": "Tanya, CMO",
+    "conditions": "", //set condition with #27
+    "choices": {
+      "a": {
+        "label": "The f%&$ you waiting for? Just do it!",
+        "values": {
+          "money": 10,
+          "environment": 5,
+          "time": -2,
+        }
+      },
+      "b": {
+        "label": "Don't waste my time.",
+        "values": {
+          "money": 0,
+          "environment": -5,
+          "time": 0,
+        }
+      }
+    }
+  },
+{
+    "id": 27,
+    "description": "Small scale or large scale development of our new tidal power system?",
+    "character": "Tanya, CMO",
+    "conditions": "",
+    "choices": {
+      "a": {
+        "label": "Meh, small scale will do.",
+        "values": {
+          "money": -10,
+          "environment": 5,
+          "time": -2,
+        }
+      },
+      "b": {
+        "label": "GO BIG OR GO HOME!",
+        "values": {
+          "money": -30,
+          "environment": 10,
+          "time": -3,
+        }
+      }
+    }
+  },
+{
+    "id": 28,
+    "description": "Would you like it in a mug or paper cups?",
+    "character": "Jeff, StarBugz Barista",
+    "conditions": "",
+    "choices": {
+      "a": {
+        "label": "Mug please.",
+        "values": {
+          "money": -5,
+          "environment": 5,
+          "time": 0,
+        }
+      },
+      "b": {
+        "label": "Paper cup duh.",
+        "values": {
+          "money": -5,
+          "environment": -5,
+          "time": 0,
+        }
+      }
+    }
+  },
+{
+    "id": 29,
+    "description": "Hey Test-Lah, we are going to build a model solar powered island city. Would you like to join us?",
+    "character": "Specific Electric",
+    "conditions": "",
+    "choices": {
+      "a": {
+        "label": "Any publicity is good publicity",
+        "values": {
+          "money": 10,
+          "environment": 5,
+          "time": -3,
+        }
+      },
+      "b": {
+        "label": "Me no speak-uh ing-gur-rish",
+        "values": {
+          "money": 0,
+          "environment": 5,
+          "time": 0,
+        }
+      }
+    }
+  },
+{
+    "id": 30,
+    "description": "Honey, shall we have another baby?",
+    "character": "Dearest Spouse",
+    "conditions": "",
+    "choices": {
+      "a": {
+        "label": "What about we adopt one? Have you looked on E-Buy?",
+        "values": {
+          "money": -50,
+          "environment": 5,
+          "time": -1,
+        }
+      },
+      "b": {
+        "label": "* smiles * One more for a football team?",
+        "values": {
+          "money": -50,
+          "environment": -5,
+          "time": -2,
+        }
+      }
+    }
+  },
+
+]
+
+var choice;
+var temp;
+var arr = [];
+
+function initialize(order){
+  x = 6;
+  arr.push(x);
+  temp = cards[x];
+  $("#description").text(temp.description);
+  $("#name").text(temp.character);
+  if (order==1){
+      choice = [temp.choices.a.label,temp.choices.a.values.money,temp.choices.a.values.environment,temp.choices.a.values.time];
+    $("#a").text(choice[0]);
+    $("#b").text(temp.choices.b.label);
+  }
+  else if(order==2){
+      choice = [temp.choices.b.label,temp.choices.b.values.money,temp.choices.b.values.environment,temp.choices.b.values.time];
+    $("#b").text(choice[0]);
+    $("#a").text(temp.choices.a.label);
+  }
+}
+
+
+function update(order){
+  money = Number(choice[1])+Number($("#money").text());
+  environment = Number(choice[2])+Number($("#environment").text());
+  time = Number($("#time").text())+Number(choice[3]);
+  $("#description").fadeOut("fast",function() {
+  $(this).text(temp.description).fadeIn("fast");
+  });
+  //$("#description").text(temp.description);
+  var avatar = "img/avatar/"+temp.character+".png";
+  $("#avatar2").fadeOut("fast",function() {
+  $("#avatar2").attr("src", avatar).fadeIn("fast");
+  });
+  $("#name").text(temp.character);
+  if (order==1){
+    choice = [temp.choices.a.label,temp.choices.a.values.money,temp.choices.a.values.environment,temp.choices.a.values.time];
+    $("#a").text(choice[0]);
+    $("#b").text(temp.choices.b.label);
+  }
+  else if(order==2){
+    choice = [temp.choices.b.label,temp.choices.b.values.money,temp.choices.b.values.environment,temp.choices.b.values.time];
+    $("#b").text(choice[0]);
+    $("#a").text(temp.choices.a.label);
+  }
+  $("#money").text(money);
+  $("#environment").text(environment);
+  $("#time").text(time);
+}
+
+$(document).ready(function(){
+  initialize(1);
+  //$("#myModal").hide();
+});
+
+$(document).ready(function(){
+    $("#a").click(function(){
+        if(arr.length > 26){
+          arr = [];
+        }
+        x = Math.floor((Math.random() * 28));
+        while($.inArray(x, arr) > -1){
+          x = Math.floor((Math.random() * 28));
+        }
+        arr.push(x);
+        temp = cards[x];
+        update(1);
+        if(Number($("#time").text())<=0 || Number($("#environment").text()) <=0 || Number($("#money").text()) <=0){
+          //window.close();
+          //location.reload();
+          $("#money2").text($("#money").text());
+          $("#environment2").text($("#environment").text());
+          $(document).ready(function(){$("#myModal2").modal('show');});
+        }
+    });
+});
+
+$(document).ready(function(){
+    $("#b").click(function(){
+        if(arr.length > 26){
+          arr = [];
+        }
+        x = Math.floor((Math.random() * 28));
+        while($.inArray(x, arr) > -1){
+          x = Math.floor((Math.random() * 28));
+        }
+        arr.push(x);
+        temp = cards[x];
+        update(2);
+        if(Number($("#time").text())<=0 || Number($("#environment").text()) <=0 || Number($("#money").text()) <=0){
+          //window.close();
+          //location.reload();
+          $("#money2").text($("#money").text());
+          $("#environment2").text($("#environment").text());
+          $(document).ready(function(){$("#myModal2").modal('show');});
+        }
+    });
+});
